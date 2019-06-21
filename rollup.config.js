2043b974db4059166c4fbcb3ad35ba657e9ec5f9
wykhuh <wykhuh@users.noreply.github.com>
2019-06-21 06:05:41 -0700

    install babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 import { uglify } from "rollup-plugin-uglify";
+import babel from "rollup-plugin-babel";
 
 export default {
   input: "src/index.js",
@@ -7,15 +8,18 @@ export default {
     format: "iife",
     name: "template",
     file: "template.js",
-    sourcemap: true,
+    sourcemap: true
   },
   plugins: [
     nodeResolve(),
     uglify(),
+    babel({
+      exclude: "node_modules/**" // only transpile our source code
+    })
   ],
   /* Cyclic dependencies are allowed in ES6, and such imports occur
      in many d3 components, so suppress those rollup warnings. */
-  onwarn: function (warning, warn) {
+  onwarn: function(warning, warn) {
     if (warning.code === "CIRCULAR_DEPENDENCY") return;
     warn(warning);
   }
